caa172b1ad485bff0f41f007eb68912e22b83e16
Frosty2d <micaiasd263@gmail.com>
2025-05-09 22:04:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://micaias240022yuri.vercel.app'
-}));
 app.use((req, res, next) => {
  res.setHeader("Access-Control-Allow-Origin", "*");
  res.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, PATCH, DELETE');
